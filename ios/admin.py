04a8c69bb1be0a1ff0d58a7fc638d65d3b362ba3
qiangwei1992 <wei.qianghb@foxmail.com>
2015-09-28 14:07:31 +0800

    finish

--- ios/admin.py
+++ ios/admin.py
@@ -22,10 +22,26 @@ class StatusAdmin(admin.ModelAdmin):
     search_fields = ('tel',)
 
 admin.site.register(status.Status, StatusAdmin)
-admin.site.register(praise_status.PraiseStatus)
+
+class PraiseAdmin(admin.ModelAdmin):
+    list_display = ('status_id', 'tel')
+    search_fields = ('status_id', 'tel')
+
+admin.site.register(praise_status.PraiseStatus, PraiseAdmin)
+
 admin.site.register(status_pic.StatusPics)
-admin.site.register(attention_relation.AttentionRelation)
-admin.site.register(comment.Comment)
+
+class AttentAdmin(admin.ModelAdmin):
+    list_display = ('attent_tel', 'tel_by_attent')
+    search_fields = ('attent_tel', 'tel_by_attent')
+
+admin.site.register(attention_relation.AttentionRelation, AttentAdmin)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('commenter', 'comment_by','comment_content')
+    search_fields = ('commenter', 'comment_by','comment_content')
+
+admin.site.register(comment.Comment, CommentAdmin)
 
 class CharAdmin(admin.ModelAdmin):
     list_display = ('sender', 'reciver', 'content', 'readed')
@@ -34,11 +50,18 @@ class CharAdmin(admin.ModelAdmin):
 admin.site.register(chat.Chat, CharAdmin)
 
 class JobAdmin(admin.ModelAdmin):
-    list_display = ('title', 'position', 'sponsor_name')
-    search_fields = ('title', 'position', 'sponsor_name')
+    list_display = ('title', 'position', 'time', 'user_time', 'appliers', 'approve_applier')
+    list_filter = ('title', 'time', 'user_time', 'approve_applier', 'status')
+    search_fields = ('title',)
 
 admin.site.register(job.Job, JobAdmin)
-admin.site.register(apply_out.ApplyOut)
+
+class ApplyOutAdmin(admin.ModelAdmin):
+    list_display = ('tel','start_time','end_time','reason')
+    search_fields = ('tel','start_time','end_time','reason')
+    list_filter = ('tel','start_time','end_time','reason')
+
+admin.site.register(apply_out.ApplyOut, ApplyOutAdmin)
 
 #admin.site.register(home_page.HomePage)
 #admin.site.register(about_fumeng.AboutFumeng)
